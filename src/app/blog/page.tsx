666de22cadd6bac0766f0696bdb646d6fe5f44ad
Int314 <15305383+Int314@users.noreply.github.com>
2025-03-16 06:56:58 +0900

    ブログ一覧ページ作成

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { posts } from "../../data/posts";
+
+export default function BlogPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">ブログ一覧</h1>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.id} className="mb-2">
+              <Link
+                href={`/blog/${post.slug}`}
+                className="text-blue-500 hover:underline"
+              >
+                {post.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
